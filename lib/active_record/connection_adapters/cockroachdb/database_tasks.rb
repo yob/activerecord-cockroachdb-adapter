caa3eb2f8689f0566158ab2a4f144a5c23991d54
James Healy <james@yob.id.au>
2021-06-06 23:54:07 +1000

    Add bare minimal support for dumping and loading the database structure
    This works for me, but would need a lot more care and attention to be a general solution for other folks as well

--- lib/active_record/connection_adapters/cockroachdb/database_tasks.rb
+++ lib/active_record/connection_adapters/cockroachdb/database_tasks.rb
@@ -5,11 +5,44 @@ module ActiveRecord
     module CockroachDB
       class DatabaseTasks < ActiveRecord::Tasks::PostgreSQLDatabaseTasks
         def structure_dump(filename, extra_flags=nil)
-          raise "db:structure:dump is unimplemented. See https://github.com/cockroachdb/activerecord-cockroachdb-adapter/issues/2"
+          args = [
+            "sql",
+            "--execute", "show create all tables",
+            "--database", db_config.database,
+            "--format=table",
+            "--insecure",
+            {:out=>filename},
+          ]
+          run_cmd("cockroach", args, "dumping")
+          remove_sql_fluff(filename)
         end
 
         def structure_load(filename, extra_flags=nil)
-          raise "db:structure:load is unimplemented. See https://github.com/cockroachdb/activerecord-cockroachdb-adapter/issues/2"
+          puts "loading from: #{filename}"
+          args = [
+            "sql",
+            "--database", db_config.database,
+            "--insecure",
+            {:in=>filename},
+          ]
+          run_cmd("cockroach", args, "loading")
+        end
+
+        private
+
+        def remove_sql_fluff(filename)
+          tempfile = Tempfile.open("uncommented_structure.sql")
+          begin
+            File.foreach(filename) do |line|
+              if (!line.start_with?("------") && !line[/\A\s+create_statement\s+\z/] && !line.strip.end_with?("rows)"))
+                tempfile << line
+                removing_comments = false
+              end
+            end
+          ensure
+            tempfile.close
+          end
+          FileUtils.cp(tempfile.path, filename)
         end
       end
     end
